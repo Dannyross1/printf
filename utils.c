bc5bae0612eda0f3dd522d9f10dd72f92269c69a
Dannyross1 <ukohdaniel@gmail.com>
2023-04-24 07:28:31 -0700

    Update

--- utils.c
+++ utils.c
@@ -15,7 +15,7 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * append_hexa_code - Append ascci in hexadecimal code to buffer.
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
@@ -38,7 +38,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Verifies if a char is a digit
+ * is_digit - Verifies if a char is a digit.
  * @c: Char to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
@@ -56,7 +56,7 @@ int is_digit(char c)
  * @num: Number to be casted.
  * @size: Number indicating the types to be casted.
  *
- * Return: Casted value of num
+ * Return: Casted value of num.
  */
 long int convert_size_number(long int num, int size)
 {
@@ -70,10 +70,10 @@ long int convert_size_number(long int num, int size)
 
 /**
  * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number to be casted
+ * @num: Number to be casted.
  * @size: Number indicating the types to be casted
  *
- * Return: Casted value of num
+ * Return: Casted value of num.
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
