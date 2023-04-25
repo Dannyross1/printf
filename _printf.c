1c0e138c1ebdf8263ca6358ce230a3087b7125ed
Desuwaa <125489934+Desuwaa@users.noreply.github.com>
2023-04-25 18:55:58 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,9 +3,9 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf functions
- * @format: format.
- * Return: Printed characters.
+ * _printf - Printf function to print in console
+ * @format: array to print and check type
+ * Return: count of character printed
  */
 int _printf(const char *format, ...)
 {
